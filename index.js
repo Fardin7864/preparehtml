996598a4f6559fc8ffd572db1aa768a1e617df22
khandokarsumiya <khandokarsumiya@gmail.com>
2023-08-19 01:06:13 +0600

    fun script add

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+function setElementById(inputId,textId){
+    const inputText = document.getElementById(inputId);
+    const outputText = document.getElementById(textId);
+    outputText.innerText = inputText.value;
+    inputText.value = '';
+
+    }
+    
+document.getElementById('comment-btn').addEventListener('click',(e) => { 
+    setElementById('name-input','name-output');
+    setElementById('email-input','email-output');
+    setElementById('mobile-input','mobile-output');
+    e.preventDefault();
+ })
